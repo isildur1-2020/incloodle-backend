18089a9712064afbd05a5604ccbe00224a6da714
Esteban G <tapitarias@gmail.com>
2022-12-26 17:31:15 -0800

    rename the endpoint production

--- src/config/variables.js
+++ src/config/variables.js
@@ -1,7 +1,7 @@
 const variables = {
   host:
     process.env.NODE_ENV === "production"
-      ? "https://incloodle.herokuapp.com"
+      ? "https://incloodle.fly.dev"
       : "http://127.0.0.1:64578",
 };
 
